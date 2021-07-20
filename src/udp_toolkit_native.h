6d2fa14245cc06a985a7ad23f4a0c86a6fe71a0c
timofei.myagkikh <t.myagkikh@protonmail.com>
2021-07-20 19:49:45 +0300

    add send method

--- src/udp_toolkit_native.h
+++ src/udp_toolkit_native.h
@@ -47,13 +47,11 @@ UDP_TOOLKIT int udp_toolkit_cleanup() {
 #endif
 }
 
-UDP_TOOLKIT void udp_toolkit_print_ip(Ipv4Address* address)
-{
+UDP_TOOLKIT void udp_toolkit_print_ip(Ipv4Address* address) {
     printf("address %d port %d \n", address->ipv4, address->port);
 }
 
-UDP_TOOLKIT int udp_toolkit_create()
-{
+UDP_TOOLKIT int udp_toolkit_create() {
     int domain   = AF_INET;
     int type     = SOCK_DGRAM;
     int protocol = IPPROTO_UDP;
@@ -61,16 +59,14 @@ UDP_TOOLKIT int udp_toolkit_create()
     return socket(domain, type, protocol);
 }
 
-UDP_TOOLKIT int udp_toolkit_receive(int socket, Ipv4Address* address, uint8_t* buffer, int length)
-{
+UDP_TOOLKIT int udp_toolkit_receive(int socket, Ipv4Address* address, uint8_t* buffer, int length) {
     struct sockaddr_storage source = { 0 };
     socklen_t addressLength = sizeof(source);
 
 
     int bytes = recvfrom(socket, (char*)buffer, length, 0, (struct sockaddr*)&source, &addressLength);
 
-     if (address != NULL)
-     {
+     if (address != NULL) {
          struct sockaddr_in* socketAddress = (struct sockaddr_in*)&source;
          address->ipv4 = socketAddress->sin_addr.s_addr;
          address->port = htons(socketAddress->sin_port);
@@ -79,8 +75,7 @@ UDP_TOOLKIT int udp_toolkit_receive(int socket, Ipv4Address* address, uint8_t* b
     return bytes;
 }
 
-UDP_TOOLKIT int udp_toolkit_bind(int socket, const Ipv4Address* address)
-{
+UDP_TOOLKIT int udp_toolkit_bind(int socket, const Ipv4Address* address) {
     struct sockaddr_in addr = {
         .sin_family = AF_INET,
         .sin_port = htons(address->port),
@@ -90,8 +85,7 @@ UDP_TOOLKIT int udp_toolkit_bind(int socket, const Ipv4Address* address)
     return bind(socket, (struct sockaddr *) &addr, sizeof(addr));
 }
 
-UDP_TOOLKIT int udp_toolkit_poll(int socket, long timeout)
-{
+UDP_TOOLKIT int udp_toolkit_poll(int socket, long timeout) {
     fd_set set = { 0 };
     struct timeval time = { 0 };
 
@@ -104,12 +98,20 @@ UDP_TOOLKIT int udp_toolkit_poll(int socket, long timeout)
     return select(socket + 1, &set, NULL, NULL, &time);
 }
 
-UDP_TOOLKIT void udp_toolkit_close(int *socket)
-{
-    if (socket > 0)
-    {
+UDP_TOOLKIT void udp_toolkit_close(int *socket) {
+    if (socket > 0) {
         close(*socket);
 
         *socket = 0;
     }
+}
+
+UDP_TOOLKIT int udp_toolkit_send(int socket, const Ipv4Address* address, const uint8_t* buffer, int length) {
+    struct sockaddr_in dest = {
+        .sin_family = AF_INET,
+        .sin_port = htons(address->port),
+        .sin_addr.s_addr = address->ipv4,
+    };
+
+    return sendto(socket, (const char*)buffer, length, 0, (address != NULL ? (struct sockaddr*)&dest : NULL), sizeof(dest));
 }
