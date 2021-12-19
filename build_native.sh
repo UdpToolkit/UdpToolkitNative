0ca0e3b6f6e732d76f411345651d90c30c4a2ff5
Timofey Myagkikh <rdcm@protonmail.ch>
2021-12-19 21:54:01 +0300

    ios build with clang

--- build_native.sh
+++ build_native.sh
@@ -7,20 +7,17 @@ then
     -DCMAKE_TOOLCHAIN_FILE=$NDK_PATH/build/cmake/android.toolchain.cmake \
     -DANDROID_ABI=$ABI \
     -DANDROID_PLATFORM=android-$MINSDKVERSION
+  cmake --build ./build/$TARGET_OS -v
 elif [ "$TARGET_OS" == "ios" ];
 then
-  cmake -H. -B build/$TARGET_OS \
-      -DCMAKE_BUILD_TYPE=Release \
-      -G Xcode -B build/$TARGET_OS \
-      -DCMAKE_SYSTEM_NAME=iOS \
-      -DCMAKE_Swift_COMPILER_FORCED=true \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0
+  mkdir -p ./build/ios/
+  clang -arch armv7 -arch arm64 -c ./src/udp_toolkit_native.c -o ./build/ios/udp_toolkit_native.so -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 
 # ios toolchain:
 # https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-ios-tvos-or-watchos
 else
   cmake -H. -B build/$TARGET_OS \
   -DCMAKE_BUILD_TYPE=Release
+  cmake --build ./build/$TARGET_OS -v
 fi
 
-cmake --build ./build/$TARGET_OS -v
