39d992f1f0a79c3c3017f93938d16f40eee7eb59
Timofey Myagkikh <rdcm@protonmail.ch>
2021-12-25 17:41:04 +0300

    publish dev nuget package

--- build_nuget.sh
+++ build_nuget.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# desktop platforms
 mkdir -p ./UdpToolkit.Network.Native/runtimes/win-x86/native
 mkdir -p ./UdpToolkit.Network.Native/runtimes/win-x64/native
 mkdir -p ./UdpToolkit.Network.Native/runtimes/osx-x64/native
 mkdir -p ./UdpToolkit.Network.Native/runtimes/linux-x64/native
 mkdir -p ./UdpToolkit.Network.Native/runtimes/linux-arm64/native
 
-mv ./udp_toolkit_native.dll   ./UdpToolkit.Network.Native/runtimes/win-x64/native
-mv ./udp_toolkit_native.dylib ./UdpToolkit.Network.Native/runtimes/osx-x64/native
-mv ./udp_toolkit_native.so    ./UdpToolkit.Network.Native/runtimes/linux-x64/native
+# mobile platforms
+mkdir -p ./UdpToolkit.Network.Native/runtimes/arm-v7/native
+mkdir -p ./UdpToolkit.Network.Native/runtimes/arm-v8/native
+mkdir -p ./UdpToolkit.Network.Native/runtimes/ios/native
+
+# desktop platforms
+mv ./win-x64/udp_toolkit_native.dll     ./UdpToolkit.Network.Native/runtimes/win-x64/native
+mv ./mac-x64/udp_toolkit_native.dylib   ./UdpToolkit.Network.Native/runtimes/osx-x64/native
+mv ./linux-x64/udp_toolkit_native.so    ./UdpToolkit.Network.Native/runtimes/linux-x64/native
+
+# mobile platforms
+mv ./android-armv7/udp_toolkit_native.so    ./UdpToolkit.Network.Native/runtimes/android-armv7/native
+mv ./android-armv8/udp_toolkit_native.so    ./UdpToolkit.Network.Native/runtimes/android-armv8/native
+mv ./ios/udp_toolkit_native.so              ./UdpToolkit.Network.Native/runtimes/ios/native
 
 dotnet pack ./UdpToolkit.Network.Native/UdpToolkit.Network.Native.csproj -c Release -o ./
